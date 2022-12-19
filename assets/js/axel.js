d0a10768b624790b82a28c6dcac2c0547015d88d
Victor Lopez <victorlpz2301@gmail.com>
2022-12-19 12:53:44 +0100

    moves

--- assets/js/axel.js
+++ assets/js/axel.js
@@ -15,34 +15,44 @@ class Axel {
       this.quiet.src ="../assets/resources/Axel1.png"
       this.quiet.frames = 3
       this.quiet.frameIndex = 0
-      this.tick2 = 0
+      this.quiet.tick = 0
 
       this.walking = new Image()
       this.walking.src ="../assets/resources/axelMove.png"
       this.walking.frames = 4
       this.walking.frameIndex = 0
-      this.tick = 0
+      this.walking.tick = 0
 
       this.back = new Image()
       this.back.src ="../assets/resources/reversemove.png"
       this.back.frames = 4
       this.back.frameIndex = 0
-      this.tick = 0
+      this.back.tick = 0
 
       this.jumpImg = new Image()
-      this.jumpImg.src ="../resources/jump.png"
+      this.jumpImg.src ="../../assets/resources/jump.png"
       this.jumpImg.frames = 2
       this.jumpImg.frameIndex = 0
-      this.tick = 0
+      this.jumpImg.tick = 0
+
+      this.punching = new Image()
+      this.punching.src ="../../assets/resources/punch.png"
+      this.punching.frames = 3
+      this.punching.frameIndex = 0
+      this.punching.tick = 0
+      this.animatePunchTick = 0
 
       this.jumpAudio = new Audio('../assets/resources/jump.wav')
       this.jumpAudio.volume = 0.5
+
+      this.isPunching = false
     }
 
     draw() {
       this.ctx.imageSmoothingEnabled = false
-     if (this.vx === 0) {
-      this.ctx.drawImage(this.quiet,
+     if (this.vx === 0 && this.y === this.y0 && this.isPunching === false) {
+      this.ctx.drawImage(
+        this.quiet,
         this.quiet.frameIndex * this.quiet.width / this.quiet.frames,
         0,
         this.quiet.width / this.quiet.frames,
@@ -52,7 +62,7 @@ class Axel {
         this.w,
         this.h)
        
-     } if (this.vx > 0) {
+     } else if (this.vx > 0 && this.y === this.y0) {
       this.ctx.drawImage(
         this.walking,
         this.walking.frameIndex * this.walking.width / this.walking.frames,
@@ -64,8 +74,7 @@ class Axel {
         this.w,
         this.h)
         this.animate()
-     }
-     if (this.vx < 0) {
+     } else if (this.vx < 0 && this.y === this.y0) {
       this.ctx.drawImage(
         this.back,
         this.back.frameIndex * this.back.width / this.back.frames,
@@ -77,9 +86,10 @@ class Axel {
         this.w,
         this.h)
         this.animate3()
-    }
-    if(this.vy < -10) {
-      this.ctx.drawImage(this.jumpImg,
+    } 
+    if(this.y < this.y0) {
+      this.ctx.drawImage(
+        this.jumpImg,
         this.jumpImg.frameIndex * this.jumpImg.width / this.jumpImg.frames,
         0,
         this.jumpImg.width / this.jumpImg.frames,
@@ -89,8 +99,27 @@ class Axel {
         this.w,
         this.h)
         this.animateJump()
+    } if(this.isPunching === true) {
+      this.punching.tick++
+      this.ctx.drawImage(
+        this.punching,
+        this.punching.frameIndex * this.punching.width / this.punching.frames,
+        0,
+        this.punching.width / this.punching.frames,
+        this.punching.height,
+        this.x,
+        this.y,
+        this.w,
+        this.h)
+        this.punch()
+      if (this.punching.tick > 45) {
+        this.punching.tick = 0
+        this.isPunching = false
+      }
+     
     }
   }
+
 
     move() {
      this.vx += this.ax
@@ -101,24 +130,20 @@ class Axel {
      if (this.y >= this.y0) {
       this.y = this.y0
       this.vy = 0
-    }
-
-    if (this.x <= 0) {
+    } else if (this.x <= 0) {
       this.vx = 0
       this.x = 0
-    }
-
-    if (this.x + this.w >= this.ctx.canvas.width) {
+    } else if (this.x + this.w >= this.ctx.canvas.width) {
       this.vx = 0
       this.x = this.ctx.canvas.width - this.w
     }
     }
 
     animate() {
-      this.tick++
+      this.walking.tick++
       
-      if (this.tick > 15) {
-          this.tick = 0
+      if (this.walking.tick > 15) {
+          this.walking.tick = 0
           this.walking.frameIndex++
       
           if (this.walking.frameIndex > this.walking.frames - 1) {
@@ -140,9 +165,9 @@ class Axel {
     // }
 
     animate3() {
-      this.tick2++
-      if (this.tick2 > 15) {
-        this.tick2 = 0
+      this.back.tick++
+      if (this.back.tick > 15) {
+        this.back.tick = 0
         this.back.frameIndex++
     
         if (this.back.frameIndex > this.back.frames - 1) {
@@ -152,12 +177,12 @@ class Axel {
     }
 
     animateJump() {
-      this.tick++
-      if (this.tick > 15) {
-          this.tick = 0
+      this.jumpImg.tick++
+      if (this.jumpImg.tick > 15) {
+          this.jumpImg.tick = 0
           this.jumpImg.frameIndex++
       
-          if (this.jumpImg.frameIndex > this.jumpImg.frames - 1) {
+          if (this.jumpImg.frameIndex > this.jumpImg.frames -1) {
               this.jumpImg.frameIndex = 0
             }  
       }
@@ -170,6 +195,23 @@ class Axel {
       }
    }
 
+   punch() {
+    this.animatePunchTick++
+    if (this.animatePunchTick > 15) {
+      this.animatePunchTick = 0
+      this.punching.frameIndex++
+  
+      if (this.punching.frameIndex > this.punching.frames - 1) {
+          this.punching.frameIndex = 0
+        }  
+    }
+  }
+
+
+    
+     
+
+
     onKeyDown(key) {
       switch(key) {
         case RIGHT:
@@ -180,6 +222,9 @@ class Axel {
           break;
         case UP:
           this.jump()
+          break;
+        case SPACE:
+          this.isPunching = true
           break;
       }
     }
--- assets/js/life.js
+++ assets/js/life.js
@@ -5,11 +5,6 @@ class Life {
         this.y = 0
         this.w = 0
         this.h = 0
-
-        const wid = 200
-        const hei = 20
-        const lifeMax = 100
-        let actualLife = 50
     }
 
 
@@ -21,9 +16,13 @@ class Life {
         this.ctx.strokeRect(0, 0, this.ctx.canvas.width, 60)
         this.ctx.fillStyle = 'white'
         this.ctx.font = '15px sans-serif'
-        this.ctx.fillText('PRESS 2UP START', 400, 50)
+        this.ctx.fillText('PRESS 2UP START', 400, 35)
         this.ctx.font = '20px sans-serif'
         this.ctx.fillText('TIME', 280, 20)
+        this.ctx.fillStyle = 'white'
+        this.ctx.fillRect(20, 30, 200, 10)
+        this.ctx.fillStyle = 'darkred'
+        this.ctx.fillRect(20, 30, 100, 10)
 
     }
 
